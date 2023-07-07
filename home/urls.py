d8ef639646850477b73883900c440445a2c7702d
unknown <hegdenagashree19@gmail.com>
2023-07-07 22:16:05 +0530

    project started

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('home', views.home),
+    path('authorized', views.authorized),
+    
+]
